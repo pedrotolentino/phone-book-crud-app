6c48796c4559470ba547af160b96cd884d302760
Pedro <pedro_tolentino12@hotmail.com>
2023-05-04 12:40:33 -0300

    Removing unused imports

--- phone-book-actions/src/main/java/com/phone/book/app/actions/controller/ContactController.java
+++ phone-book-actions/src/main/java/com/phone/book/app/actions/controller/ContactController.java
@@ -4,7 +4,6 @@ import com.phone.book.app.actions.model.Contact;
 import com.phone.book.app.actions.model.wrapper.ContactWrapper;
 import com.phone.book.app.actions.service.ContactService;
 import jakarta.validation.Valid;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
@@ -12,8 +11,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/v1/contacts")
--- phone-book-actions/src/main/java/com/phone/book/app/actions/exception/CustomExceptionHandler.java
+++ phone-book-actions/src/main/java/com/phone/book/app/actions/exception/CustomExceptionHandler.java
@@ -2,14 +2,10 @@ package com.phone.book.app.actions.exception;
 
 import com.phone.book.app.actions.exception.custom.ResourceNotFoundException;
 import com.phone.book.app.actions.model.Error;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 
